606aa153113e89c640e10a588751c073435c46da
Vito Gentile <gentilevito1076@gmail.com>
2021-01-27 15:17:55 +0100

    VUE Caricato In HTML ed Instanziato

--- main.js
+++ main.js
@@ -5,3 +5,8 @@ In questo caso dovete modificare l'html (semplificarlo di molto!) perché ora ci
 Consigli:
 Sdraticate da tutte le sinapsi ( :smile: ) ciò che jquery ci aveva insegnato. Ricordiamoci che con Vue.js comanda il dato.
 I dati infatti non li prendiamo più dall'HTML ma li creiamo e gestiamo noi. Poi l'html andrà a recuperarli dal dato stesso.*/
+
+// INSTANZIAMENTO DI VUE:
+new Vue({
+ el: '#root'
+});
