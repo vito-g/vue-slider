a8befd9cc5ce81997a1a073e96bdad33ab1691ea
Vito Gentile <gentilevito1076@gmail.com>
2021-01-27 20:04:48 +0100

    Reinserita dell'immagine "perduta"...

--- main.js
+++ main.js
@@ -14,7 +14,8 @@ new Vue({
     images : [
       'https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=clouds-country-daylight-371633.jpg&fm=jpg',
       'https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg',
-      'https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg'
+      'https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg',
+      'https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg'
     ],
   },
   methods : {
