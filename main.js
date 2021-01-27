043449ba4bfbd6fb54d16b30ebec06bce26a2951
Vito Gentile <gentilevito1076@gmail.com>
2021-01-27 17:16:50 +0100

    Sistemate le vecchie classi

--- main.js
+++ main.js
@@ -18,7 +18,6 @@ new Vue({
       'davide'
       ],
     imgIndex: 0,
-    imgWidth: '4400px',
     images : [
       'https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=clouds-country-daylight-371633.jpg&fm=jpg',
       'https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg',
