df929743cf321121c0f794ce49809e7bb9c6a136
Vito Gentile <gentilevito1076@gmail.com>
2021-01-27 16:24:37 +0100

    Array URL immagini + Struttura Html + FX

--- main.js
+++ main.js
@@ -10,8 +10,29 @@ I dati infatti non li prendiamo più dall'HTML ma li creiamo e gestiamo noi. Poi
 new Vue({
  el: '#root',
  data: {
-   message : 'HELLO!'
- }
+     message : 'HELLO!',
+     people: [
+      'gianpaolo',
+      'giuseppe',
+      'teresa',
+      'davide'
+      ],
+    imgIndex: 0,
+    images : [
+      'https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=clouds-country-daylight-371633.jpg&fm=jpg',
+      'https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg',
+      'https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg',
+      'https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg'
+    ],
+  },
+  methods : {
+  next: function() {
+  this.imgIndex += 1;
+  },
+  prev: function() {
+  this.imgIndex -= 1;
+  }
+}
 });
 
 Vue.config.devtools = true;
