1282bb8fc88619b45e7166069009337605986665
Vito Gentile <gentilevito1076@gmail.com>
2021-01-27 13:48:31 +0100

    File Preparati

---
+++ main.js
@@ -0,0 +1,7 @@
+/*Obiettivo:
+Ricreare lo slider già fatto in jquery con Vue.js.
+Scaricate da qui la repository: https://bitbucket.org/booleancareers/lc-ex-slider-layout/downloads/
+In questo caso dovete modificare l'html (semplificarlo di molto!) perché ora ci penserà vue.js!
+Consigli:
+Sdraticate da tutte le sinapsi ( :smile: ) ciò che jquery ci aveva insegnato. Ricordiamoci che con Vue.js comanda il dato.
+I dati infatti non li prendiamo più dall'HTML ma li creiamo e gestiamo noi. Poi l'html andrà a recuperarli dal dato stesso.*/
